d086d8775c6268aca567ef5786b1f0c9ab1bb1aa
sahilkhan89 <sahilk.bca2021@ssism.org>
2023-12-06 13:02:55 +0530

    test

--- Backend/src/main/java/digital_board/digital_board/Repository/SportRepository.java
+++
@@ -1,9 +0,0 @@
-package digital_board.digital_board.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import digital_board.digital_board.Entity.Sport;
-
-public interface SportRepository extends JpaRepository<Sport, String>{
-    
-}
--- Backend/src/main/java/digital_board/digital_board/Servies/ResultAnnouncmentService.java
+++
@@ -1,6 +0,0 @@
-package digital_board.digital_board.Servies;
-
-public interface ResultAnnouncmentService {
-
-   
-}
